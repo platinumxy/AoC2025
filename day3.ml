b437635b5de3bbd15f12c3a34d7b3c948a04abaa
Archie S <me@platinumxy.com>
2025-12-04 13:15:15 +0000

    fmt

--- day3.ml
+++ day3.ml
@@ -13,34 +13,34 @@ let rec get_max = function
       let mx, rem = get_max xs in
       if x >= mx then (x, xs) else (mx, rem)
 
-let without_last ?(n = 1) lst =
-  let len = List.length lst in
-  if n >= len then (lst, [])
-  else
-    let rec aux acc count = function
-      | [] -> (List.rev acc, [])
+  let without_last ?(n = 1) lst =
+    let len = List.length lst in
+    if n >= len then (lst, [])
+    else
+      let rec aux acc count = function
+        | [] -> (List.rev acc, [])
+        | x :: xs ->
+            if count < len - n then aux (x :: acc) (count + 1) xs
+            else (List.rev acc, x :: xs)
+      in
+      aux [] 0 lst
+
+  let max_for n vals =
+    let rec aux count = function
+      | [] -> failwith "List shorter than n"
       | x :: xs ->
-          if count < len - n then aux (x :: acc) (count + 1) xs
-          else (List.rev acc, x :: xs)
+          let usable, unusable = without_last ~n:(n - count) (x :: xs) in
+          let mx, dropped = get_max usable in
+          mx :: (if count == n then [] else aux (count + 1) (dropped @ unusable))
     in
-    aux [] 0 lst
+    merge (aux 1 vals)
 
-let max_for n vals =
-  let rec aux count = function
-    | [] -> failwith "List shorter than n"
-    | x :: xs ->
-        let usable, unusable = without_last ~n:(n - count) (x :: xs) in
-        let mx, dropped = get_max usable in
-        mx :: (if count == n then [] else aux (count + 1) (dropped @ unusable))
-  in
-  merge (aux 1 vals)
-
-let solve_with fn lines =
-  List.to_seq lines
-  |> Seq.map (fun s -> String.to_seq s |> List.of_seq)
-  |> Seq.map atomoise |> Seq.map fn |> Seq.map int_of_string
-  |> Seq.fold_left ( + ) 0
-  |> fun x -> x
+  let solve_with fn lines =
+    List.to_seq lines
+    |> Seq.map (fun s -> String.to_seq s |> List.of_seq)
+    |> Seq.map atomoise |> Seq.map fn |> Seq.map int_of_string
+    |> Seq.fold_left ( + ) 0
+    |> fun x -> x
 
 let part1 = solve_with (max_for 2)
 let part2 = solve_with (max_for 12)
